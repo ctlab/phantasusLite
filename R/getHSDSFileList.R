3e4299f3be7dbbf90115a3bb0f50e205ebdf5fd6
Alexey Sergushichev <alsergbox@gmail.com>
2024-07-22 14:32:56 -0500

    fix getHSDSFileList

--- R/getHSDSFileList.R
+++ R/getHSDSFileList.R
@@ -1,3 +1,22 @@
+listDomainFiles <- function(src, directory) {
+    request <- paste0(src@endpoint, "/domains?domain=", directory)
+    response <- rhdf5client:::submitRequest(request)
+    domains <- response[["domains"]]
+    
+    res <- lapply(domains, function(d) {
+        if (d$class == "domain") {
+            return(d$name)
+        }
+        
+        stopifnot(d$class == "folder")
+        return(c(d$name,
+                 listDomainFiles(src, d$name)))
+    })
+    res <- unlist(res)    
+    res
+}
+
+
 #' Returns list of all HDF5-files on HSDS-server
 #' @param url, containing url of the server and root domain.
 #' @param directory, containing name of the directory
@@ -13,36 +32,16 @@
 
 getHSDSFileList <- function(url='https://alserglab.wustl.edu/hsds/?domain=/counts', directory = NULL) {
     src <- httr::parse_url(url)
-    dir <- src$query$domain
+    root <- src$query$domain
     src <- paste0(src$scheme,'://',src$hostname,'/',src$path)
     src <- HSDSSource(src)
-    hdf5FileList  <- list()
     if (is.null(directory)) {
-      directories <- listDomains(src, dir)
-      directories <- directories[-grep("*\\.h5$", directories)]
-      directories <- gsub(paste0(dir, '/'), '', directories)
-      for (directory in directories) {
-        request <- paste0(src@endpoint, "/domains?domain=",
-                          dir, '/', directory)
-        response <- rhdf5client:::submitRequest(request)
-        domains <- response[["domains"]]
-        for (domain in domains) {
-          if (domain$name != paste0(dir, "/", directory, '/', directory, ".h5")) {
-            hdf5FileList <- append(hdf5FileList, domain$name)
-          }
-        }
-      }
+        directory <- root
     } else {
-      request <- paste0(src@endpoint, "/domains?domain=",
-                        dir, '/', directory)
-      response <- rhdf5client:::submitRequest(request)
-      domains <- response[["domains"]]
-      for (domain in domains) {
-        if (domain$name != paste0(dir, "/", directory, '/', directory, ".h5")) {
-          hdf5FileList <- append(hdf5FileList, domain$name)
-        }
-      }
+        directory <- paste0(root, "/", directory)
     }
-    hdf5FileList <- unlist(hdf5FileList)
+    
+    hdf5FileList  <- listDomainFiles(src, directory)
+    hdf5FileList <- grep(".*\\.h5$", hdf5FileList, value = TRUE)
     return(hdf5FileList)
 }
